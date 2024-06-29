d952f32f7c31d0abe4817dfd4dff70a9e7ca97d1
Hemant Garg <108003083+hemantgarg1452@users.noreply.github.com>
2024-06-29 10:15:56 +0530

    Time: 117 ms (71.57%), Space: 163.7 MB (12.18%) - LeetHub

---
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(n);
+        for (auto it : edges) {
+            adj[it[1]].push_back(it[0]);
+        }
+        vector<vector<int>> ans;
+
+        for (int i = 0; i < n; ++i) {
+            queue<int> q;
+            q.push(i);
+            vector<int> v;
+            vector<int> vis(n, 0);
+
+            while (!q.empty()) {
+                for (auto it : adj[q.front()]) {
+                    if (!vis[it]) {
+                        q.push(it);
+                        v.push_back(it);
+                        vis[it] = 1;
+                    }
+                }
+                q.pop();
+            }
+
+            sort(v.begin(), v.end());
+
+            ans.push_back(v);
+        }
+
+        return ans;
+    }
+};
