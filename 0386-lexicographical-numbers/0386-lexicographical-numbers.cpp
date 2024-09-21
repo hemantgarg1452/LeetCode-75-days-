be5ed03a767e9b0b74d089c448f20b2d561348a8
Hemant Garg <108003083+hemantgarg1452@users.noreply.github.com>
2024-09-21 17:00:18 +0530

    Time: 423 ms (5.05%), Space: 16.1 MB (36.01%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -3,10 +3,17 @@ public:
     vector<int> lexicalOrder(int n) {
         vector<int> vect;
         for(int i=1; i<=n; ++i) vect.push_back(i);
-        
+
         sort(vect.begin(), vect.end(), [](int left, int right){
             return to_string(left)<to_string(right);
         });
         return vect;
     }
 };
+
+static const int KDS = []() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
